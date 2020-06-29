4cd093e5768e5e23618732e2f19d7b891a8e13af
sjelzinga <sje.elzinga@gmail.com>
2020-06-29 11:55:08 +0200

    Add show photo info

--- src/components/images/ImageDetails/ImageDetails.tsx
+++ src/components/images/ImageDetails/ImageDetails.tsx
@@ -22,11 +22,17 @@ export const ImageDetails: React.FC<IProps> = ({ match }) => {
     window.scrollTo(0, 0);
     axiosClient
       .get(`/photos/${match.params.id}`)
-      .then((res: AxiosResponse<PhotoObject>) => setImage(res.data))
+      .then((res: AxiosResponse<PhotoObject>) => {
+        setImage(res.data);
+      })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  if (!image) {
+    return <div>No data</div>;
+  }
 
   const find = () => {
     findEdges(imgRef);
@@ -35,8 +41,19 @@ export const ImageDetails: React.FC<IProps> = ({ match }) => {
   return (
     <div className="image-details">
       <img ref={imgRef} src={image?.urls.regular} alt="" className="image" />
+      <div className="image-info">
+        {!image.user ? (
+          <div></div>
+        ) : (
+          <React.Fragment>
+            <div className="name"> {image.user.name}</div>
+            <div className="created-at">{image.created_at}</div>
+            <div className="description">{image.description}</div>
+          </React.Fragment>
+        )}
+      </div>
       <button onClick={find}>Find Edges</button>
-      <div>
+      <div className="canvas-wrapper">
         <canvas id="output" className="image-canvas" />
       </div>
     </div>
